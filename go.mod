b69fdf696fdc25313002dd2abe2b43fc525fac24
Arseniy Kucher <beeffallo@gmail.com>
2019-12-11 15:17:40 +0200

    twenty48

--- go.mod
+++ go.mod
@@ -4,19 +4,8 @@ go 1.12
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-	github.com/gopherjs/gopherjs v0.0.0-20191106031601-ce3c9ade29de // indirect
-	github.com/hajimehoshi/ebiten v1.9.3
-	github.com/hajimehoshi/wasmserve v0.0.0-20190923141630-74336f2566c1 // indirect
-	github.com/kisielk/gotool v1.0.0 // indirect
-	github.com/neelance/astrewrite v0.0.0-20160511093645-99348263ae86 // indirect
-	github.com/neelance/sourcemap v0.0.0-20151028013722-8c68805598ab // indirect
-	github.com/shurcooL/go v0.0.0-20190704215121-7189cc372560 // indirect
-	github.com/shurcooL/go-goon v0.0.0-20170922171312-37c2f522c041 // indirect
-	github.com/shurcooL/goexec v0.0.0-20181215213458-76a6b9a99126 // indirect
-	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749 // indirect
-	github.com/spf13/cobra v0.0.5 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/hajimehoshi/ebiten v1.10.1
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	golang.org/x/crypto v0.0.0-20191029031824-8986dd9e96cf // indirect
-	golang.org/x/sys v0.0.0-20191029155521-f43be2a4598c // indirect
+	golang.org/x/image v0.0.0-20191206065243-da761ea9ff43
 )
--- main.go
+++ main.go
@@ -1,161 +1,50 @@
+// Copyright 2016 The Ebiten Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build example jsgo
+
 package main
 
 import (
-	"fmt"
 	"log"
-	"math/rand"
-	"time"
 
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
-)
-
-// World represents the game state.
-type World struct {
-	area   []bool
-	width  int
-	height int
-}
-
-// NewWorld creates a new world.
-func NewWorld(width, height int, maxInitLiveCells int) *World {
-	w := &World{
-		area:   make([]bool, width*height),
-		width:  width,
-		height: height,
-	}
-	w.init(maxInitLiveCells)
-	return w
-}
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
-// init inits world with a random state.
-func (w *World) init(maxLiveCells int) {
-	for i := 0; i < maxLiveCells; i++ {
-		x := rand.Intn(w.width)
-		y := rand.Intn(w.height)
-		w.area[y*w.width+x] = true
-	}
-}
-
-// Update game state by one tick.
-func (w *World) Update() {
-	width := w.width
-	height := w.height
-	next := make([]bool, width*height)
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			pop := neighbourCount(w.area, width, height, x, y)
-			switch {
-			case pop < 2:
-				// rule 1. Any live cell with fewer than two live neighbours
-				// dies, as if caused by under-population.
-				next[y*width+x] = false
-
-			case (pop == 2 || pop == 3) && w.area[y*width+x]:
-				// rule 2. Any live cell with two or three live neighbours
-				// lives on to the next generation.
-				next[y*width+x] = true
-
-			case pop > 3:
-				// rule 3. Any live cell with more than three live neighbours
-				// dies, as if by over-population.
-				next[y*width+x] = false
-
-			case pop == 3:
-				// rule 4. Any dead cell with exactly three live neighbours
-				// becomes a live cell, as if by reproduction.
-				next[y*width+x] = true
-			}
-		}
-	}
-	w.area = next
-}
-
-// Draw paints current game state.
-func (w *World) Draw(pix []byte) {
-	for i, v := range w.area {
-		if v {
-			pix[4*i] = 0xff
-			pix[4*i+1] = 0xff
-			pix[4*i+2] = 0xff
-			pix[4*i+3] = 0xff
-		} else {
-			pix[4*i] = 0
-			pix[4*i+1] = 0
-			pix[4*i+2] = 0
-			pix[4*i+3] = 0
-		}
-	}
-}
-
-func max(a, b int) int {
-	if a < b {
-		return b
-	}
-	return a
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-// neighbourCount calculates the Moore neighborhood of (x, y).
-func neighbourCount(a []bool, width, height, x, y int) int {
-	c := 0
-	for j := -1; j <= 1; j++ {
-		for i := -1; i <= 1; i++ {
-			if i == 0 && j == 0 {
-				continue
-			}
-			x2 := x + i
-			y2 := y + j
-			if x2 < 0 || y2 < 0 || width <= x2 || height <= y2 {
-				continue
-			}
-			if a[y2*width+x2] {
-				c++
-			}
-		}
-	}
-	return c
-}
-
-const (
-	scale        = 1
-	screenWidth  = 320 * 1
-	screenHeight = 240 * 1
+	"github.com/hajimehoshi/ebiten/examples/2048/2048"
 )
 
 var (
-	world  = NewWorld(screenWidth, screenHeight, int((screenWidth*screenHeight)/10))
-	pixels = make([]byte, screenWidth*screenHeight*4)
+	game *twenty48.Game
 )
 
 func update(screen *ebiten.Image) error {
-	world.Update()
-
+	if err := game.Update(); err != nil {
+		return err
+	}
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
-
-	world.Draw(pixels)
-	screen.ReplacePixels(pixels)
-
-	msg := fmt.Sprintf("TPS: %0.2f\nFPS: %0.2f", ebiten.CurrentTPS(), ebiten.CurrentFPS())
-	ebitenutil.DebugPrint(screen, msg)
-
+	game.Draw(screen)
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, screenWidth, screenHeight, scale, "Game of Life (Ebiten Demo)"); err != nil {
+	var err error
+	game, err = twenty48.NewGame()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := ebiten.Run(update, twenty48.ScreenWidth, twenty48.ScreenHeight, 1, "2048 (Ebiten Demo)"); err != nil {
 		log.Fatal(err)
 	}
 }
